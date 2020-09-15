02d1acedcc902f9af9ee1c4b3ecf3b1ef21f31eb
arisukarno <arisukarno.25@gmail.com>
2020-09-15 23:14:32 +0700

    add exercise2

---
+++ exercise2.sql
@@ -0,0 +1,11 @@
+-- search the items name, count of sale,price and total income of profit PROGATEID website
+SELECT items.name AS "Nama Items",
+       COUNT(*) AS "Jumlah Penjualan",
+       items.price AS "Harga",
+       COUNT(*)*items.price AS "Total Pemasukan"
+FROM sales_records
+JOIN items
+ON sales_records.item_id=items.id
+GROUP BY items.price, items.name
+ORDER BY COUNT(*)*items.price DESC
+LIMIT 5 ;
