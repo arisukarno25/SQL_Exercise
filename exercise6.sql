f578bd2d00a811179c2cbea9451d7efa54cde8ea
ari sukarno <arisukarno.25@gmail.com>
2020-12-04 10:25:01 +0700

    add exercise 6

---
+++ exercise6.sql
@@ -0,0 +1,9 @@
+SELECT items.name AS "Nama Item",
+       items.price - items.cost AS "Keuntungan Per Item",
+       1.0 * (items.price - items.cost) / (items.price)*100 AS "Presentase Keuntungan Per Item",
+       COUNT(*)*(items.price - items.cost) AS "Keuntungan Total Per Item"
+FROM sales_records
+JOIN items ON sales_records.item_id=items.id
+GROUP BY "Nama Item", "Keuntungan Per Item", "Presentase Keuntungan Per Item"
+ORDER BY COUNT(*)*"Keuntungan Per Item" ASC
+LIMIT 5;
