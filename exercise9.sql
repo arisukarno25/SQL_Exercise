f0b9f24d8984b529eb9d1639c4d6d5d20324aa3a
arisukarno <arisukarno.25@gmail.com>
2021-02-01 14:59:00 +0700

    update exercise 9

--- exercise9.sql
+++ exercise9.sql
@@ -1,5 +1,16 @@
-SELECT 1.0 * COUNT(*) / COUNT(DISTINCT(sales_records.user_id)) AS "Frekuensi Rata-Rata Pembelian",
-       1.0 * SUM(items.price) / COUNT(DISTINCT(sales_records.user_id)) AS "Pengeluaran Rata-Rata",
-       (1.0 * SUM(items.price) / COUNT(DISTINCT(sales_records.user_id))) / (1.0 * COUNT(*) / COUNT(DISTINCT(sales_records.user_id))) AS "Pengeluaran Rata-Rata per Pembelian"
+SELECT 1.0 * COUNT(*) / COUNT(DISTINCT(sales_records.user_id)) AS "rata_jumlah_pembelian",
+       1.0 * SUM(items.price) / COUNT(DISTINCT(sales_records.user_id)) AS "rata_pengeluaran",
+       (1.0 * SUM(items.price) / COUNT(DISTINCT(sales_records.user_id))) / (1.0 * COUNT(*) / COUNT(DISTINCT(sales_records.user_id))) AS "rata_pengeluaran_per_belanjaan"
 FROM sales_records
 JOIN items ON sales_records.item_id=items.id
+
+--OR
+SELECT rata_jumlah_pembelian,
+       rata_pengeluaran,
+       1.0 * rata_pengeluaran / rata_jumlah_pembelian AS rata_pengeluaran_per_belanjaan
+FROM (
+    SELECT 1.0 * COUNT(*) / COUNT(DISTINCT(sales_records.user_id)) AS rata_jumlah_pembelian,
+    1.0 * SUM(items.price) / COUNT(DISTINCT(sales_records.user_id)) AS rata_pengeluaran
+FROM sales_records
+JOIN items ON sales_records.item_id=items.id
+)
